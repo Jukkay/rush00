5b1d02b1381d6cb78fd9c2a67b1a9b3a49181446
Jukka Ylimaula <jylimaul@student.hive.fi>
2022-04-16 15:19:49 +0300

    fixed password change

--- modif.php
+++ modif.php
@@ -3,11 +3,11 @@ if ($_POST['submit'] != 'OK' || $_POST['login'] == "" || $_POST['oldpw'] == "" |
 	echo "ERROR\n";
 	return;
 }
-$filename = '../private/passwd';
+$filename = './private/passwd';
 $array = unserialize(file_get_contents($filename));
 $oldpw = hash("whirlpool", $_POST['oldpw']);
 foreach ($array as &$user) {
-	if ($user['login'] == $_POST['login'] && $user['passwd'] == $oldpw) {
+	if ($user['login'] == $_POST['login'] && $user['passwd'] == hash("whirlpool", $_POST['oldpw'])) {
 		$user['passwd'] = hash("whirlpool", $_POST['newpw']);
 		file_put_contents($filename, serialize($array));
 		echo "OK\n";
